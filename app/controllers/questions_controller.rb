f2b7c7b3ba2c895deb1c7bf6f517dad3fe121a20
Jan Krejcik <jan-krejcik@outlook.com>
2023-01-30 21:03:14 +0100

    added styling with bootstrap

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,13 +5,14 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
 
-    @answer = ""
-      if @question == ("I am going to work right now!" || "I am going to work right now!".upcase)
-        return @answer
-      elsif (@question.count "?") >= 1
-        return @answer = "Silly question, get dressed and go to work!"
-      else
-        return @answer = "I don't care, get dressed and go to work!"
-      end
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif (@question.count "?") >= 1
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question.blank?
+      @answer = 'I can`t hear you!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
   end
 end
